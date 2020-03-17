fddb89239a3d4aac54fa010bb362e66e34a76610
Julien Gourmelen <julien.gourmelen@coopengo.com>
2020-03-17 15:15:36 +0100

    update

---
+++ init-user-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER coog WITH ENCRYPTED PASSWORD 'coog';
+	CREATE DATABASE coog;
+	GRANT ALL PRIVILEGES ON DATABASE coog TO coog;
+    CREATE EXTENSION unaccent;
+    ALTER DATABASE coog OWNER TO coog;
+EOSQL
