e96c2621b66114c841bdb3276a8953e383e4e4d5
Jean Cavallo <jean.cavallo@hotmail.fr>
2021-07-01 18:29:37 +0200

    Clean

--- postgres/init-user-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER coog WITH ENCRYPTED PASSWORD 'coog' LOGIN;
-	CREATE DATABASE coog OWNER coog;
-        CREATE EXTENSION unaccent;
-        CREATE DATABASE coog2 OWNER coog; 
-EOSQL
