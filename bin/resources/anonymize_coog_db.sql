0c6b97a7b5d6b65ba1776186bca4a1df9fd9effd
Antoine Pinston <antoine.pinston@coopengo.com>
2026-01-05 18:33:12 +0100

    Init

--- bin/resources/anonymize_coog_db.sql
+++ bin/resources/anonymize_coog_db.sql
@@ -73,7 +73,7 @@ BEGIN
     PERFORM anon_table('account_payment', '', 'description');
     PERFORM anon_table('event_log', '', 'description');
     PERFORM anon_table('api_token', 'name, key', 'request_hash');
-    PERFORM anon_table('ir_api_identity', 'identifier');
+    PERFORM anon_table('ir_api_identity', 'identifier', '', 'id: NOT IN :(select a.id from ir_api_identity a inner join res_user b ON b.id = a.user inner join "res_user-res_group" c ON c.user = b.id inner join res_group d ON d.id = c.group WHERE d.name = ''Coog Do Not Anonymize'')');
     PERFORM anon_table('contract_agira_request', '', '', '', 'applicant, deceased_person');
     PERFORM anon_table('contract_agira_beneficiary', '', '', '', 'beneficiary');
 
